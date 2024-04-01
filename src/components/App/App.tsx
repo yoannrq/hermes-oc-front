25d3a495bb23f70f32c62f111ecf78f1b13677ee
Yoan GAUSSENS <yoan.gaussens@oclock.school>
2024-04-01 15:37:34 +0200

    wip: Creating login page (html)

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./LoginPage/Header/Header";
+import Form from "./LoginPage/Form/Form";
+import Footer from "./LoginPage/Footer/Footer";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/App/LoginPage/Form/Form.tsx
@@ -0,0 +1,14 @@
+const Form = () => {
+  return(
+    <form>
+      <div>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+      </div>
+        <button type="submit">Login</button>
+    </form>
+  )
+};
+
+export default Form;
+  

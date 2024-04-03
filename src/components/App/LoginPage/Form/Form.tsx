3120d03b12e93b93a76556d9fd46de0e20316a7d
Yoan GAUSSENS <yoan.gaussens@oclock.school>
2024-04-03 12:33:40 +0200

    feat: Adding "Forgot password" and "Sign up" to loggin page

--- src/components/App/LoginPage/Form/Form.tsx
+++ src/components/App/LoginPage/Form/Form.tsx
@@ -1,6 +1,8 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
 
 const Form = () => {
   return (
@@ -27,6 +29,18 @@ const Form = () => {
           <Button variant="contained">Connexion</Button>
         </div>
       </Box>
+      <Grid container sx={{display: "flex", justifyContent: "space-around", width: '40ch'}}>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="#" variant="body2">
+            Don't have an account? Sign Up
+          </Link>
+        </Grid>
+      </Grid>
     </>
   );
 };

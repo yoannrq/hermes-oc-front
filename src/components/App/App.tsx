efc8f42c1b49bd76a4e8da3517999646ecdb22bb
Yoan GAUSSENS <yoan.gaussens@oclock.school>
2024-04-03 17:30:35 +0200

    ci: Config modifications

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
-import Header from './LoginPage/Header/Header';
-import Form from './LoginPage/LoginForm/LoginForm';
-import Footer from './LoginPage/Footer/Footer';
+import Header from '../LoginPage/Header/Header';
+import Form from '../LoginPage/LoginForm/LoginForm';
+import Footer from '../LoginPage/Footer/Footer';
 import './App.scss';
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SignUpForm from './components/SignUpPage/SignUpForm'
 // import App from './components/App/App.tsx'
+import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

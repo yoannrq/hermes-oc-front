56697c7a40197e7e0923847458f6b1d8f0cb88f1
Yoan GAUSSENS <yoan.gaussens@oclock.school>
2024-04-09 10:17:55 +0200

    refactor: Refactoring snackbar component

--- src/pages/SignUp/components/SignUpForm.tsx
+++ src/pages/SignUp/components/SignUpForm.tsx
@@ -1,35 +1,41 @@
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
+import {
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+  Link,
+  Snackbar,
+  Box,
+} from '@mui/material';
 
-import CustomToast from './CustomToast';
 import React, { useState } from 'react';
-
 import backend from './../../../utils/backend.ts';
-import { fieldsConfig } from './FormFields.ts';
 
 export interface SignUpFromProps {
   onRequireLogin: () => void;
 }
 
 function SignUpForm({ onRequireLogin }: SignUpFromProps) {
-  const [errorMessage, setErrorMessage] = useState("");
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
+  let formDataIsValid = true;
+  const passwordMatch = password === confirmPassword;
 
-  let formDataIsValid = true
-  const passwordMatch = password === confirmPassword
-
-  if(!firstname || !lastname || !email || !password || !confirmPassword || !passwordMatch){
-    formDataIsValid = false
+  if (
+    !firstname ||
+    !lastname ||
+    !email ||
+    !password ||
+    !confirmPassword ||
+    !passwordMatch
+  ) {
+    formDataIsValid = false;
   }
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -39,36 +45,45 @@ function SignUpForm({ onRequireLogin }: SignUpFromProps) {
       firstname,
       lastname,
       email,
-      password
+      password,
     });
 
     if (res.ok) {
       onRequireLogin();
     } else {
-      const errorsByFieldName : {[key: string]: string[]} = {}
+      const errorsByFieldName: { [key: string]: string[] } = {};
 
       if (res.data.error.errors) {
-        res.data.error.errors.map((error) => {
+        interface Error {
+          path: string[];
+          message: { value: string };
+        }
+
+        res.data.error.errors.map((error: Error) => {
           const fieldName = error.path[0]; // "password" | "email" | ...
           const errorMessage = error.message.value as string;
 
-          const fieldErrors = errorsByFieldName[fieldName] || []
+          const fieldErrors = errorsByFieldName[fieldName] || [];
 
-          fieldErrors.push(errorMessage.replace(`The field '${fieldName}'`, ''))
-          errorsByFieldName[fieldName] = fieldErrors
+          fieldErrors.push(
+            errorMessage.replace(`The field '${fieldName}'`, '')
+          );
+          errorsByFieldName[fieldName] = fieldErrors;
         });
 
-        let recapError = ""
+        let recapError = '';
 
-        for ( const fieldName of Object.keys(errorsByFieldName)){
-          const fieldErros = errorsByFieldName[fieldName]
-          recapError += `The field '${fieldName}' contains : ${fieldErros.length} error${fieldErros.length > 1 ? "s": ""}.` 
+        for (const fieldName of Object.keys(errorsByFieldName)) {
+          const fieldErros = errorsByFieldName[fieldName];
+          recapError += `The field '${fieldName}' contains : ${
+            fieldErros.length
+          } error${fieldErros.length > 1 ? 's' : ''}.`;
 
           fieldErros.forEach((msg) => {
-            recapError += `\n\t${msg}`
-          })
+            recapError += `\n\t${msg}`;
+          });
 
-          recapError += "\n\n"
+          recapError += '\n\n';
         }
 
         setErrorMessage(recapError);
@@ -108,41 +123,90 @@ function SignUpForm({ onRequireLogin }: SignUpFromProps) {
           }}
         >
           <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} >
-                <TextField type="text" autoComplete='fname' name='firstname' label='Prénom' id='firstname' required={true} fullWidth={true} autoFocus={true} 
-                  onChange={(e) => {setFirstname(e.target.value)}}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6} >
-                <TextField type='text' autoComplete='lname' name='lastname' label='Nom' id='lastname' required={true} fullWidth={true} 
-                  onChange={(e) => {setLastname(e.target.value)}}
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField type='email' autoComplete='email' name='email' label='Adresse mail' id='email' required={true} fullWidth={true} 
-                  onChange={(e) => {setEmail(e.target.value)}}
-                />
-              </Grid>
-              {/* RPPS code is not required */}
-              {/* <Grid item xs={12} >
+            <Grid item xs={12} sm={6}>
+              <TextField
+                type="text"
+                autoComplete="fname"
+                name="firstname"
+                label="Prénom"
+                id="firstname"
+                required={true}
+                fullWidth={true}
+                autoFocus={true}
+                onChange={(e) => {
+                  setFirstname(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                type="text"
+                autoComplete="lname"
+                name="lastname"
+                label="Nom"
+                id="lastname"
+                required={true}
+                fullWidth={true}
+                onChange={(e) => {
+                  setLastname(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                autoComplete="email"
+                name="email"
+                label="Adresse mail"
+                id="email"
+                required={true}
+                fullWidth={true}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </Grid>
+            {/* //* RPPS code is not required for v1 */}
+            {/* <Grid item xs={12} >
                 <TextField type='text' name='rpssCode' label='Numero RPPS' id='rppsCode' required={true} fullWidth={true} 
                   onChange={(e) => {setRpss(e.target.value)}}
                 />
               </Grid> */}
-              <Grid item xs={12} >
-                <TextField type='password' autoComplete='new-password' name='password' label='Mot de passe' id="password" required={true} fullWidth={true} 
-                  onChange={(e) => {setPassword(e.target.value)}}
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField type='password' autoComplete='new-password' name='confirmPassword' label='Confirmez le mot de passe' id='confirmPassword' required={true} fullWidth={true} 
-                  onChange={(e) => {setConfirmPassword(e.target.value)}}
-                />
-                {!passwordMatch &&
-                  <Typography textAlign="left" sx={{my: "0.4em"}}>Les mots de passes doivent être identiques.</Typography>
-                }
-              </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="password"
+                autoComplete="new-password"
+                name="password"
+                label="Mot de passe"
+                id="password"
+                required={true}
+                fullWidth={true}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="password"
+                autoComplete="new-password"
+                name="confirmPassword"
+                label="Confirmez le mot de passe"
+                id="confirmPassword"
+                required={true}
+                fullWidth={true}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+              />
+              {!passwordMatch && (
+                <Typography textAlign="left" sx={{ my: '0.4em' }}>
+                  Les mots de passes doivent être identiques.
+                </Typography>
+              )}
+            </Grid>
           </Grid>
+
           <Grid sx={{ mt: 3 }}>
             <Button
               disabled={!formDataIsValid}
@@ -154,10 +218,30 @@ function SignUpForm({ onRequireLogin }: SignUpFromProps) {
             </Button>
           </Grid>
 
-          <CustomToast
-            open={errorMessage !== ""}
+          <Snackbar
+            sx={{
+              height: 'auto',
+              lineHeight: '28px',
+              padding: 24,
+              whiteSpace: 'pre-wrap',
+            }}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={errorMessage !== ''}
+            autoHideDuration={6000}
+            onClose={() => setErrorMessage('')}
             message={errorMessage}
-            onClose={() => setErrorMessage("")}
+            action={
+              <Button
+                color="secondary"
+                size="small"
+                onClick={() => setErrorMessage('')}
+              >
+                Fermer
+              </Button>
+            }
           />
 
           <Grid sx={{ mt: 2, display: 'flex', justifyContent: 'end' }}>

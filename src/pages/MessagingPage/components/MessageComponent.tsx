c8a9ce4bf91a28be7d54fba6fa460e170f845162
Yoan GAUSSENS <yoan.gaussens@oclock.school>
2024-04-16 14:09:48 +0200

    feat: Adding the ability to edit a message

--- src/pages/MessagingPage/components/MessageComponent.tsx
+++ src/pages/MessagingPage/components/MessageComponent.tsx
@@ -1,6 +1,5 @@
-import { Box, Button, Snackbar } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+import { Box, Button, Snackbar, TextField } from '@mui/material';
+import { Delete, Edit, Check, Clear } from '@mui/icons-material';
 import { useState, useEffect, useRef } from 'react';
 
 import { useUserContext } from '../../../contexts/userContext';
@@ -24,8 +23,10 @@ function MessageComponent({ message }: MessageComponentProps) {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [showButton, setShowButton] = useState(false);
 
-  const isAuthorMessage = user.id === message.authorId;
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(message.content);
 
+  const isAuthorMessage = user.id === message.authorId;
 
   const boxRef = useRef<HTMLElement>(null);
 
@@ -42,13 +43,52 @@ function MessageComponent({ message }: MessageComponentProps) {
     };
   }, []);
 
-
   const handleClick = async () => {
     setShowButton(true);
   };
 
   const handleModifyClick = () => {
     console.log('Le bouton "Modifier" a été cliqué !');
+
+    setIsEditing(true);
+  };
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedContent(event.target.value);
+  };
+
+  const handleSaveEdit = async () => {
+    setIsEditing(false);
+
+    try {
+      const response = await fetch(`/api/me/messages/${message.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content: editedContent,
+        }),
+      });
+
+      if (response.ok) {
+        console.log(`Le message ${message.id} a été modifié avec succès !`);
+      } else {
+        console.error(
+          'Erreur lors de la modification du message (else):',
+          response.status
+        );
+      }
+    } catch (error) {
+      console.error(
+        'Erreur lors de la modification du message (catch):',
+        error
+      );
+    }
+  };
+
+  const handleCloseEdit = () => {
+    setIsEditing(false);
   };
 
   const handleOpenSnackbar = () => {
@@ -61,8 +101,6 @@ function MessageComponent({ message }: MessageComponentProps) {
 
   const handleDeleteClick = async () => {
     console.log('Le bouton "Supprimer" a été cliqué !');
-
-    handleOpenSnackbar();
 
     try {
       const response = await fetch(`/api/me/messages/${message.id}`, {
@@ -97,12 +135,16 @@ function MessageComponent({ message }: MessageComponentProps) {
         message="Voulez-vous vraiment supprimer ce message ?"
         action={
           <>
-          <Button color="secondary" size="small" onClick={handleCloseSnackbar}>
-            Annuler
-          </Button>
-          <Button color="secondary" size="small" onClick={handleDeleteClick}>
-            Supprimer
-          </Button>
+            <Button
+              color="secondary"
+              size="small"
+              onClick={handleCloseSnackbar}
+            >
+              Annuler
+            </Button>
+            <Button color="secondary" size="small" onClick={handleDeleteClick}>
+              Supprimer
+            </Button>
           </>
         }
       ></Snackbar>
@@ -110,15 +152,18 @@ function MessageComponent({ message }: MessageComponentProps) {
       {!isAuthorMessage && (
         <AvatarComponent src="https://mui.com/static/images/avatar/3.jpg" />
       )}
-      <MessageBubble
-        message={message}
-        onClick={() => {
+      <Box sx={{ position: 'relative' }}>
+        <MessageBubble
+          message={message}
+          onClick={() => {
           if (isAuthorMessage) handleClick();
         }}
-        style={{ cursor: isAuthorMessage ? 'pointer' : 'default' }}
-      />
+          style={{ cursor: isAuthorMessage ? 'pointer' : 'default' }}
+        />
+      </Box>
 
       {showButton && (
+        
         <Box
           sx={{
             display: 'flex',
@@ -135,7 +180,7 @@ function MessageComponent({ message }: MessageComponentProps) {
               padding: 0,
             }}
           >
-            <EditIcon />
+            <Edit />
           </Button>
           <Button
             onClick={handleOpenSnackbar}
@@ -145,7 +190,24 @@ function MessageComponent({ message }: MessageComponentProps) {
               padding: 0,
             }}
           >
-            <DeleteIcon />
+            <Delete />
+          </Button>
+        </Box>
+      )}
+      {isEditing && (
+        <Box sx={{ marginLeft: '0.5em' }}>
+          <TextField
+            value={editedContent}
+            onChange={handleContentChange}
+            fullWidth
+            multiline
+            variant="outlined"
+          />
+          <Button onClick={handleSaveEdit}>
+            <Check />
+          </Button>
+          <Button onClick={handleCloseEdit}>
+            <Clear />
           </Button>
         </Box>
       )}
